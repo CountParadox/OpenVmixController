a9074226e07c606e5c9dadd927f18248e2234f47
Lewys Martin <cp@tbof.net>
2024-08-30 11:54:22 +1000

    Create keymap.c
    add some bits for the 4x adc on rp2040

---
+++ fw/keymap.c
@@ -0,0 +1,8 @@
+//joystick config
+joystick_config_t joystick_axes[JOYSTICK_AXES_COUNT] = {
+  JOYSTICK_AXIS_IN(A0, 0, 512, 1023),
+  JOYSTICK_AXIS_IN(A1, 0, 512, 1023),
+  JOYSTICK_AXIS_IN(A2, 0, 512, 1023),
+  JOYSTICK_AXIS_IN(A3, 0, 512, 1023)
+};
+
